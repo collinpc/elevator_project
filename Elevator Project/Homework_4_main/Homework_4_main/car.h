ca63f00cbfc695e70bafd62d52de1abec26bed6f
collinpc <collinpc@mail.uc.edu>
2016-07-27 18:14:41 -0400

    Started making classes

--- Elevator Project/Homework_4_main/Homework_4_main/car.h
+++ Elevator Project/Homework_4_main/Homework_4_main/car.h
@@ -1,1 +1,44 @@
 #pragma once
+
+// knows wait times, knows how long at each floor, knows passengers
+
+#ifndef CAR.H
+#define CAR.H
+#include <iostream>
+#include <queue>
+using namespace std;
+
+
+
+class Car
+{
+private:
+
+	int travel_time_total;
+	int const time_between_floors = 3; // seconds
+	int const min_floor_time = 5; // seconds
+
+
+
+
+
+public:
+
+
+
+
+
+};
+
+
+
+
+
+
+
+
+
+
+
+
+#endif
--- Elevator Project/Homework_4_main/Homework_4_main/passenger.h
+++ Elevator Project/Homework_4_main/Homework_4_main/passenger.h
@@ -1,1 +1,64 @@
+
+// Knows where elevator is going from/to, records time for events
+
 #pragma once
+#ifndef PASSENGER.H
+#define PASSENGER.H
+using namespace std;
+
+
+class Passenger
+{
+private:
+
+	int floor_from; 
+	int floor_to;
+	int wait_total; 
+	int wait_arrival;
+
+	int arrive_time;
+	int call_time;
+	int exit_time;
+
+	int calculate_trip_time();
+	int calculate_arrival_time(); //when the elevator gets to the person
+
+
+
+
+public:
+
+
+	//setters
+
+
+	void set_wait_total(int time)
+	{
+		wait_total = time;
+	}
+
+	void set_wait_arrival(int time)
+	{
+		wait_arrival = time;
+	}
+
+
+	//Getters
+
+	int get_wait_total()
+	{
+		return wait_total;
+	}
+
+	int get_wait_arrival()
+	{
+		return wait_arrival;
+	}
+
+	
+
+
+
+};
+
+#endif
