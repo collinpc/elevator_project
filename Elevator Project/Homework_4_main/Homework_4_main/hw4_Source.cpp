4be57aa1a526b3ffed7ce25dbcb0a33652048455
collinpc <collinpc@mail.uc.edu>
2016-07-28 17:02:22 -0400

    New Change 1222

--- Elevator Project/Homework_4_main/Homework_4_main/hw4_Source.cpp
+++ Elevator Project/Homework_4_main/Homework_4_main/hw4_Source.cpp
@@ -8,6 +8,10 @@ int main()
 {
     Passenger* pass1 = new Passenger();
     pass1->calculate_trip_time();
-    cout << pass1->get_wait_total() << endl;;
+	cout << pass1->get_wait_total() << endl;
+	//i changed thisew
+
+	system("PAUSE");
     return 0;
+
 }
