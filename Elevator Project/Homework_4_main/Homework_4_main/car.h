7604406300db3accf30b6aabeb9fb7975e2da521
collinpc <collinpc@mail.uc.edu>
2016-07-28 18:52:49 -0400

    Added search tree to car.h

--- Elevator Project/Homework_4_main/Homework_4_main/car.h
+++ Elevator Project/Homework_4_main/Homework_4_main/car.h
@@ -2,10 +2,12 @@
 
 // knows wait times, knows how long at each floor, knows passengers
 
-#ifndef CAR.H
-#define CAR.H
+#ifndef CAR
+#define CAR
 #include <iostream>
 #include <queue>
+#include <map>
+#include "bst.h"
 using namespace std;
 
 
@@ -17,6 +19,20 @@ private:
 	int travel_time_total;
 	int const time_between_floors = 3; // seconds
 	int const min_floor_time = 5; // seconds
+	int const pass_load_time = 1; // Time for each passenger to load
+	int const home_floor = 3; // home floor
+	int const floor_current = 3;
+	
+	static BinarySearchTree<int, bool>*up_list = new BinarySearchTree<int, bool>; // up binary search treee
+	static BinarySearchTree<int, bool>*down_list = new BinarySearchTree<int, bool>; // down binary search tree
+
+
+
+	
+	
+
+	
+
 
 
 
@@ -24,6 +40,8 @@ private:
 
 public:
 
+	void go_up(); 
+	void go_down();
 
 
 
--- Elevator Project/Homework_4_main/Homework_4_main/hw4_Source.cpp
+++ Elevator Project/Homework_4_main/Homework_4_main/hw4_Source.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include "passenger.h"
+#include "bst.h"
+#include <cstdlib>
 
 using namespace std;
 
@@ -8,8 +10,24 @@ int main()
 {
     Passenger* pass1 = new Passenger();
     pass1->calculate_trip_time();
-	cout << pass1->get_wait_total() << endl;
-	//i changed thisew
+	cout << pass1->get_wait_total() << endl << endl;
+	//i changed thisewwe
+
+
+	BinarySearchTree<int, bool>*bst = new BinarySearchTree<int, bool>;
+
+	bst->Add(5, false);
+	bst->Add(6, true);
+
+	cout << bst->Get(5);
+
+
+	system("PAUSE");
+
+	return 0;
+
+
+
 
 	system("PAUSE");
     return 0;
